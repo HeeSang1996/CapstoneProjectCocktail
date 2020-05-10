d34fe255b0ab04cde2da69359985e095e6634599
rhs1208 <rhs1208gm@gmail.com>
2020-05-10 18:40:28 +0900

    Login Activity

---
+++ app/src/main/java/org/techtown/capstoneprojectcocktail/LoginPageActivity.java
@@ -0,0 +1,12 @@
+package org.techtown.capstoneprojectcocktail;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPageActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page_activity);
+    }
+}
