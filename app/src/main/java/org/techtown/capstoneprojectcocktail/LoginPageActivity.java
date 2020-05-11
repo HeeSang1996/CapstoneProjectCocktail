06df6ba0b3bfb29830ece9aa2cee980e7629e085
정영진 <49061702+JeongYeongJin@users.noreply.github.com>
2020-05-11 16:59:27 +0900

    구글 연동을 통한 로그인, 로그아웃 기능 추가

--- app/src/main/java/org/techtown/capstoneprojectcocktail/LoginPageActivity.java
+++ app/src/main/java/org/techtown/capstoneprojectcocktail/LoginPageActivity.java
@@ -1,62 +1,180 @@
 package org.techtown.capstoneprojectcocktail;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginPageActivity extends AppCompatActivity {
+import org.techtown.capstoneprojectcocktail.R;
+//git test
+public class LoginPageActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private TextView mStatusTextView;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page_activity);
 
-        TextInputLayout textInputLayoutForLoginID = findViewById(R.id.textInputLayoutForID_login);
-        final EditText editTextForLoginID = textInputLayoutForLoginID.getEditText();
+        // Button listeners
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        mStatusTextView = findViewById(R.id.mStatusTextView);
 
-        TextInputLayout textInputLayoutForLoginPassWord = findViewById(R.id.textInputLayoutForPassWord_login);
-        final EditText editTextForLoginPassWord = textInputLayoutForLoginPassWord.getEditText();
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
-        Button loginButtonLoginPage = findViewById(R.id.buttonForLogin_login);
-        loginButtonLoginPage.setOnClickListener(new OnSingleClickListener(){
-            @Override
-            public void onSingleClick(View view){
-                //영진 여기다가 로그인 하는 함수 박아주셈
-                //email == ID, passWord == 패스워드
-                //둘다 스트링타입
-                //프로필 사진 불러와서 이 함수 안에서 bitmap포맷으로 여기다가 채워주셈
-                //할수 있으면 profileImageView_login에 사진 직접 박아줘 ㅎ
-                String email = editTextForLoginID.getText().toString();
-                String passWord = editTextForLoginPassWord.getText().toString();
-                Snackbar.make(view, "로그인ID "+email + " 로그인패스워드 " + passWord, Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                finish();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_in_button:
+                signIn(); // 구글 로그인 버튼 클릭시
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                Log.w(TAG, "Google sign in failed", e);
+                updateUI(null);
             }
-        });
+        }
+    }
 
-        Button loginCheckButtonLoginPage = findViewById(R.id.buttonForLoginCheck_login);
-        loginCheckButtonLoginPage.setOnClickListener(new OnSingleClickListener(){
-            @Override
-            public void onSingleClick(View view){
-                //영진 여기다가 로그인 체크 출력하는 함수 박아주셈
-                Snackbar.make(view, "로그인체크: ", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
 
-        Button logoutButtonLoginPage = findViewById(R.id.buttonForLogout_login);
-        logoutButtonLoginPage.setOnClickListener(new OnSingleClickListener(){
-            @Override
-            public void onSingleClick(View view){
-                //영진 여기다가 로그아웃 하는 함수 박아주셈
-                Snackbar.make(view, "로그아웃", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.e(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            Log.e(TAG,"user getEmail : "+user.getEmail());
+                            Log.e(TAG,"user getDisplayName : "+user.getDisplayName());
+                            Log.e(TAG,"user getUid : "+user.getUid());
+                            Log.e(TAG,"user getPhoto : "+user.getPhotoUrl());
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            //Toast.makeText(GoogleSignInActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        //hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        mAuth.signOut(); // Firebase sign out
+
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        FirebaseAuth.getInstance().signOut();
+                        updateUI(null);
+                    }
+                });
+    }
+
+    private void revokeAccess() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google revoke access
+        mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        //hideProgressDialog();
+        if (user != null) {
+            mStatusTextView.setText(user.getEmail() + " : " + user.getDisplayName());
+
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText("현재 상태 : LogOut");
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //FirebaseAuth.getInstance().signOut();
     }
 }
