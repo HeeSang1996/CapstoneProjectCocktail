5f48724d8827d20a231dd805307380cf7517fd6b
sy10kr <alswnsghd15@naver.com>
2020-05-12 23:34:58 +0900

    add 그래픽 결과

--- app/src/main/java/org/techtown/capstoneprojectcocktail/MJH_SimulatorActivity.java
+++ app/src/main/java/org/techtown/capstoneprojectcocktail/MJH_SimulatorActivity.java
@@ -1,9 +1,18 @@
 package org.techtown.capstoneprojectcocktail;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -25,6 +34,7 @@ import static androidx.constraintlayout.widget.Constraints.TAG;
 public class MJH_SimulatorActivity extends AppCompatActivity {
 
     public FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Canvas canvas;
 
 
     @Override
@@ -34,25 +44,46 @@ public class MJH_SimulatorActivity extends AppCompatActivity {
 
         MJH_Object_simulator test = new MJH_Object_simulator(0,0);
 
+        Bitmap bitmap = Bitmap.createBitmap(720,1480, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        ImageView View = (ImageView) findViewById(R.id.imageView);
+        View.setImageBitmap(bitmap);
+
+        Paint paint = new Paint();
+        Paint paint_gradient = new Paint();
+
+
         Button button1 = (Button) findViewById(R.id.button1_mjh);
         Button button2 = (Button) findViewById(R.id.button2_mjh);
         Button button3 = (Button) findViewById(R.id.button3_mjh);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"1번 버튼 클릭",Toast.LENGTH_LONG).show();
+                Bitmap bitmap = Bitmap.createBitmap(720,1480, Bitmap.Config.ARGB_8888);
+                canvas = new Canvas(bitmap);
+                ImageView View = (ImageView) findViewById(R.id.imageView);
+                View.setImageBitmap(bitmap);
+                rendering2(canvas);
             }
         });
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"2번 버튼 클릭",Toast.LENGTH_LONG).show();
+                Bitmap bitmap = Bitmap.createBitmap(720,1480, Bitmap.Config.ARGB_8888);
+                canvas = new Canvas(bitmap);
+                ImageView View = (ImageView) findViewById(R.id.imageView);
+                View.setImageBitmap(bitmap);
+                rendering1(canvas);
             }
         });
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"3번 버튼 클릭",Toast.LENGTH_LONG).show();
+                Bitmap bitmap = Bitmap.createBitmap(720,1480, Bitmap.Config.ARGB_8888);
+                canvas = new Canvas(bitmap);
+                ImageView View = (ImageView) findViewById(R.id.imageView);
+                View.setImageBitmap(bitmap);
+                rendering3(canvas);
             }
         });
         //sample ingredient
@@ -175,5 +206,151 @@ public class MJH_SimulatorActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+
+    public void rendering1(Canvas canvas){
+        Paint paint = new Paint();
+        Paint paint_gradient = new Paint();
+
+        //바닥부
+        paint.setColor(Color.YELLOW);
+        RectF rect1 = new RectF();
+        rect1.set(110, 350, 650, 430);
+        canvas.drawArc(rect1, 180, 180, true, paint);
+
+        //위 사각
+        paint.setColor(Color.YELLOW);
+        canvas.drawRect(110, 380, 650, 1320, paint);
+
+        //그라데이션
+        paint_gradient.setShader(new LinearGradient(640, 620, 640, 1020, Color.YELLOW, Color.RED, Shader.TileMode.CLAMP));
+        canvas.drawRect(110, 620, 650, 1020, paint_gradient);
+
+        //아래 사각
+        paint.setColor(Color.RED);
+        canvas.drawRect(110, 1020, 650, 1320, paint);
+
+        //바닥부
+        paint.setColor(Color.RED);
+        RectF rect = new RectF();
+        rect.set(110, 1270, 650, 1370);
+        canvas.drawArc(rect, 0, 360, true, paint);
+
+
+        Bitmap bitmap2 = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.highball_glass_test_ice);
+        bitmap2 = resizeBitmapImg(bitmap2, 1480);
+        canvas.drawBitmap(bitmap2, 0, 0, null);
+
+        //빛반사
+        paint.setColor(0x56FFFFFF);
+
+        canvas.drawRect(150, 75, 300, 1370, paint);
+
+        rect.set(150, 1350, 300, 1390);
+        canvas.drawArc(rect, 90, 90, true, paint);
+    }
+
+    public void rendering2(Canvas canvas){
+        Paint paint = new Paint();
+        Paint paint_gradient = new Paint();
+
+        //바닥부
+        paint.setColor(0xEF0099FF);
+        RectF rect1 = new RectF();
+        rect1.set(110, 350, 650, 430);
+        canvas.drawArc(rect1, 180, 180, true, paint);
+
+        //위 사각
+        paint.setColor(0xEF0099FF);
+        canvas.drawRect(110, 380, 650, 1320, paint);
+
+
+        //바닥부
+        paint.setColor(0xEF0099FF);
+        RectF rect = new RectF();
+        rect.set(110, 1270, 650, 1370);
+        canvas.drawArc(rect, 0, 180, true, paint);
+
+
+        Bitmap bitmap2 = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.highball_glass_test_ice);
+        bitmap2 = resizeBitmapImg(bitmap2, 1480);
+        canvas.drawBitmap(bitmap2, 0, 0, null);
+
+        //빛반사
+        paint.setColor(0x56FFFFFF);
+
+        canvas.drawRect(150, 75, 300, 1370, paint);
+
+        rect.set(150, 1350, 300, 1390);
+        canvas.drawArc(rect, 90, 90, true, paint);
+    }
+
+    public void rendering3(Canvas canvas){
+        Paint paint = new Paint();
+        Paint paint_gradient = new Paint();
+
+        //바닥부
+        paint.setColor(0xE6FFFF00);
+        RectF rect1 = new RectF();
+        rect1.set(110, 350, 650, 430);
+        canvas.drawArc(rect1, 180, 180, true, paint);
+
+        //위 사각
+        paint.setColor(0xE6FFFF00);
+        canvas.drawRect(110, 380, 650, 620, paint);
+
+        //그라데이션
+        paint_gradient.setShader(new LinearGradient(640, 620, 640, 1020, 0xE6FFFF00, 0x96FFFFFF, Shader.TileMode.CLAMP));
+        canvas.drawRect(110, 620, 650, 1020, paint_gradient);
+
+        //아래 사각
+        paint.setColor(0x96FFFFFF);
+        canvas.drawRect(110, 1020, 650, 1320, paint);
+
+        //바닥부
+        paint.setColor(0x96FFFFFF);
+        RectF rect = new RectF();
+        rect.set(110, 1270, 650, 1370);
+        canvas.drawArc(rect, 0, 360, true, paint);
+
+
+        Bitmap bitmap2 = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.highball_glass_test_ice);
+        bitmap2 = resizeBitmapImg(bitmap2, 1480);
+        canvas.drawBitmap(bitmap2, 0, 0, null);
+
+        //빛반사
+        paint.setColor(0x56FFFFFF);
+
+        canvas.drawRect(150, 75, 300, 1370, paint);
+
+        rect.set(150, 1350, 300, 1390);
+        canvas.drawArc(rect, 90, 90, true, paint);
+    }
+    //param source 원본 Bitmap 객체
+    //param maxResolution 제한 해상도
+    //return 리사이즈된 이미지 Bitmap 객체
+    public Bitmap resizeBitmapImg(Bitmap source, int maxResolution){
+        int width = source.getWidth();
+        int height = source.getHeight();
+        int newWidth = width;
+        int newHeight = height;
+        float rate = 0.0f;
+
+        if(width > height){
+            if(maxResolution < width){
+                rate = maxResolution / (float) width;
+                newHeight = (int) (height * rate);
+                newWidth = maxResolution;
+            }
+        }else{
+            if(maxResolution < height){
+                rate = maxResolution / (float) height;
+                newWidth = (int) (width * rate);
+                newHeight = maxResolution;
+            }
+        }
+        return Bitmap.createScaledBitmap(source, newWidth, newHeight, true);
     }
 }
